4cfe3d0c248bc00ba3ea6a20788511221a96aa48
AkashSkySingh <akashpreetsingh91@gmail.com>
2017-05-27 02:04:36 -0700

    thoughts on discover page efficiency

--- frontend/components/discover/discover.jsx
+++ frontend/components/discover/discover.jsx
@@ -1,2 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+
+// Discover page will require a masonry table for all new posts, remember to optimize it.
+
+// Page will also require a masonry table for most followed users as well. Double component can potentially hamper unmounting problems. May have to split masonry tables into their own components to make mounting/unmounting easier for transitition between discover and splash page.
--- frontend/components/discover/discover_container.jsx
+++ frontend/components/discover/discover_container.jsx
@@ -2,6 +2,8 @@ import { connect } from 'react-redux';
 import Discover from './post_index';
 import { fetchPosts } from '../../actions/post_actions';
 
+// Discover page will require its own custom rails url + fixings  to make it functional. JSON views will require user information for most followed users, and most recent posts. Maybe add in user  info for most recent posts, so a user can see who uploaded the content as well.
+
 const mapStateToProps = state => ({
   posts: Object.keys(state.posts).map(id => state.posts[id])
 });
