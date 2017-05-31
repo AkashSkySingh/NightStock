378e3478cd399c72accc2dd62b01d181d2c8d6c8
AkashSkySingh <akashpreetsingh91@gmail.com>
2017-05-30 19:55:56 -0700

    addition of masonry bonus mvp components

---
+++ frontend/components/discover/new_posts.jsx
@@ -0,0 +1,1 @@
+// custom page with custom rails link to provide recent posts. Bonus MVP, requires masonry type component for post listing. Design wireframes to properly setup expectations for page design.
---
+++ frontend/components/discover/popular_users.jsx
@@ -0,0 +1,1 @@
+// custom page with custom rails link to provide most popular users. Bonus MVP, requires masonry type component for user listing. Design wireframes to properly setup expectations for page design.
