bad13c702f68ae97496b8bece79f138719ffeec6
AkashSkySingh <akashpreetsingh91@gmail.com>
2017-06-03 22:16:37 -0700

    component bonus addition

--- frontend/components/discover/discover.jsx
+++ frontend/components/discover/discover.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router';
-
+import NewPosts from './new_posts';
+import PopUsers from './popular_users';
 // Discover page will require a masonry table for all new posts, remember to optimize it.
 
 // Page will also require a masonry table for most followed users as well. Double component can potentially hamper unmounting problems. May have to split masonry tables into their own components to make mounting/unmounting easier for transitition between discover and splash page.
+
+//  Both items must be rendered in specific masonry tables to avoid demounting issue as seen and addressed above.
--- frontend/components/discover/new_posts.jsx
+++ frontend/components/discover/new_posts.jsx
@@ -1,1 +1,19 @@
 // custom page with custom rails link to provide recent posts. Bonus MVP, requires masonry type component for post listing. Design wireframes to properly setup expectations for page design.
+
+import React from 'react';
+import { hashHistory, withRouter, Link } from 'react-router';
+import Masonry from 'react-masonry-component';
+
+const masonryOptions = {
+  transition: 0
+};
+
+
+class NewPosts extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+}
+
+export default withRouter(NewPosts);
